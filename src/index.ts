09545648767c6517c3a2033c7f6e62be9faa6a83
ivansimplicio <ivansimplicio@outlook.com>
2023-10-06 02:15:34 +0000

    feat: configure apis

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import { Elysia } from "elysia";
+import app from './external/api/config'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// import { UserRepositoryInMemory } from './external/memory/userRepositoryInMemory'
+import { UserRepositoryPrisma } from './external/prisma/userRepositoryPrisma'
+
+import { CreateUserController } from './adapters/user/createUserController'
+import { ListUsersController } from './adapters/user/listUsersController'
+import { ConsultUserByIdController } from './adapters/user/consultUserByIdController'
+
+import { CreateUserUseCase } from './core/user/service/createUserUseCase'
+import { ListUsersUseCase } from './core/user/service/listUsersUseCase'
+import { ConsultUserByIdUseCase } from './core/user/service/consultUserByIdUseCase'
+
+// const userRepository = new UserRepositoryInMemory()
+const userRepository = new UserRepositoryPrisma()
+
+const createUser = new CreateUserUseCase(userRepository)
+const listUsers = new ListUsersUseCase(userRepository)
+const consultUserById = new ConsultUserByIdUseCase(userRepository)
+
+new CreateUserController(app, createUser)
+new ListUsersController(app, listUsers)
+new ConsultUserByIdController(app, consultUserById)
+
+app.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
